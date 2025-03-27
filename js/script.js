e6a1fe21968d5161d3d8074d0711ed3f078329d4
Satyamrk <satyamrk18@gmail.com>
2025-03-27 16:33:23 +0530

    internal css and js are converted into seperate files

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 function hamburgerMenu() {
-    const hamNav = document.getElementById("ham-nav");
-    hamNav.classList.toggle("show");
-  }
+  const hamNav = document.getElementById("ham-nav");
+  hamNav.classList.toggle("show");
+}
+function changeImg(imgId, changeImg) {
+  var changeMainImage = document.getElementById(imgId);
+  changeMainImage.src = changeImg;
+}
